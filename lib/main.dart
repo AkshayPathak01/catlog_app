cd679d34756b82a5b4d2f3df77bac3b3d7c19064
Akshaypathak01 <akshaypathak1520@gmail.com>
2021-06-02 22:31:08 +0530

    login page add

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,26 @@
 import 'package:catlog_app/pages/home_page.dart';
+import 'package:catlog_app/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      themeMode: ThemeMode.system,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/login": (context) => HomePage(),
+      },
+    );
   }
 }
