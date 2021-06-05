514b565e29bcfa8001ac8ab2a6bb773f339dcefd
Akshaypathak01 <akshaypathak1520@gmail.com>
2021-06-05 11:04:49 +0530

    add login button

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:catlog_app/pages/home_page.dart';
 import 'package:catlog_app/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,17 +10,23 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // buildcontext create a tree
     return MaterialApp(
-      themeMode: ThemeMode.system,
+      themeMode: ThemeMode.system, // use system ,dark ,light
       theme: ThemeData(
+        // when u use light theme then theme use
         primarySwatch: Colors.red,
+        fontFamily: GoogleFonts.lato().fontFamily,
       ),
       darkTheme: ThemeData(
+        // when u use dark theme then darktheme use
         brightness: Brightness.dark,
       ),
+      initialRoute: "/",
       routes: {
-        "/": (context) => LoginPage(),
-        "/login": (context) => HomePage(),
+        "/": (context) => new LoginPage(), // "/" by default screen of app
+        "/home": (context) =>
+            new HomePage(), // create object of class with new keyword not complusary
       },
     );
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,9 +4,66 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("log in "),
-      ),
-    );
+        //color: Colors.white,
+        child: Column(
+      children: [
+        Image.asset(
+          "assets/images/login.png",
+          fit: BoxFit.cover,
+        ),
+        SizedBox(
+          height: 20.0,
+        ),
+        Text(
+          "Welcome",
+          style: TextStyle(
+              color: Colors.black, fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(
+          height: 20.0,
+        ),
+        Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 40.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    hintText: " Enter Email ",
+                    labelText: "Email",
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(color: Colors.black38, width: 1.5),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                ),
+                TextFormField(
+                  style: TextStyle(color: Colors.white),
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: " Enter Password ",
+                    labelText: "Password",
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(color: Colors.black38, width: 1.5),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      print("Hiii");
+                    },
+                    child: Text("Login"),
+                    style: TextButton.styleFrom())
+              ],
+            ))
+      ],
+    ));
   }
 }
