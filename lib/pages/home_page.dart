a487adb263b47855229367171f4e384f0f101eeb
Akshaypathak01 <akshaypathak1520@gmail.com>
2021-06-08 22:32:02 +0530

    add json and card view

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,30 @@
+import 'package:catlog_app/Model/catlog.dart';
+import 'package:catlog_app/widgets/ItemWidget.dart';
 import 'package:catlog_app/widgets/drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catlogJson = await rootBundle.loadString("assets/files/catloJson.json");
+    print(catlogJson);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(5, (index) => CatalogModel.items[0]);
+
     return Scaffold(
       appBar: AppBar(
         // backgroundColor: Colors.white,
@@ -16,12 +37,18 @@ class HomePage extends StatelessWidget {
           color: Colors.black,
         ),*/
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome "),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
-      bottomNavigationBar: BottomAppBar(),
+      // bottomNavigationBar: BottomAppBar(),
       drawer: MyDrawer(),
     );
   }
